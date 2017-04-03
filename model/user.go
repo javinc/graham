120ed4df376c9484ebaec4532e0fcf3ebe4875d1
javinc <javinczki02@gmail.com>
2017-04-04 06:55:11 +0800

    update user model

--- model/user.go
+++ model/user.go
@@ -4,10 +4,11 @@ import "time"
 
 // User model
 type User struct {
-	ID        string     `json:"id,omitempty" field:"string"`
-	Type      string     `json:"type,omitempty" field:"string"`
-	Email     string     `json:"email,omitempty" field:"string"`
-	Active    *bool      `json:"taken,omitempty" field:"boolean"`
-	CreatedAt *time.Time `json:"created_at,omitempty"`
-	UpdatedAt *time.Time `json:"updated_at,omitempty"`
+	ID        string     `data:"id,omitempty" json:"id,omitempty"     field:"string"`
+	Type      string     `data:"type"         json:"type,omitempty"   field:"string"`
+	Name      string     `data:"name"         json:"name,omitempty"   field:"string,required"`
+	Email     string     `data:"email"        json:"email,omitempty"  field:"string"`
+	Active    int        `data:"active"       json:"active,omitempty" field:"number"`
+	CreatedAt *time.Time `data:"created_at"   json:"created_at,omitempty"`
+	UpdatedAt *time.Time `data:"updated_at"   json:"updated_at,omitempty"`
 }
