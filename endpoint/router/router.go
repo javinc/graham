c6e789c0219a58f39f3b245f7ffc2a3175b7f70a
javinc <javinczki02@gmail.com>
2017-04-04 09:05:00 +0800

    payload check

--- endpoint/router/router.go
+++ endpoint/router/router.go
@@ -1,13 +1,17 @@
 package router
 
 import (
+	"errors"
+
 	"github.com/gin-gonic/gin"
+	"github.com/javinc/mango/server/middleware"
 
 	"github.com/javinc/graham/endpoint"
 )
 
 // Routes endpoint routes
 func Routes(r *gin.Engine) {
+	// public
 	r.GET("/foo", endpoint.FindFoo)
 	r.POST("/foo", endpoint.CreateFoo)
 	r.GET("/foo/:id", endpoint.GetFoo)
@@ -16,4 +20,21 @@ func Routes(r *gin.Engine) {
 
 	r.GET("/user", endpoint.FindUser)
 	r.GET("/user/:id", endpoint.GetUser)
+
+	// private
+	p := r.Group("/", middleware.PrivateMiddleware(checkUser))
+	{
+		p.GET("/foox", endpoint.FindFoo)
+	}
+}
+
+func checkUser(payload map[string]interface{}) error {
+	id := payload["id"].(string)
+
+	// check if id exists
+	if id != "testUser" {
+		return errors.New("THIS IS INVALID USER")
+	}
+
+	return nil
 }
