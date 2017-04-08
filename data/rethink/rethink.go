c4c7d98c1eb242d0d1fb24ff14b1687f0a5383a3
javinc <javinczki02@gmail.com>
2017-04-08 11:25:40 +0800

    fix var names

--- data/rethink/rethink.go
+++ data/rethink/rethink.go
@@ -3,12 +3,12 @@ package rethink
 import (
 	r "github.com/gorethink/gorethink"
 	"github.com/javinc/mango/config"
-	db "github.com/javinc/mango/database/rethink"
+	"github.com/javinc/mango/database/rethink"
 )
 
 // init db connection
 func init() {
-	db.Connect(db.Config{
+	rethink.Connect(rethink.Config{
 		Host:    config.GetString("rethink.host"),
 		Db:      config.GetString("rethink.db"),
 		MaxOpen: config.GetInt("rethink.max_open"),
@@ -17,37 +17,37 @@ func init() {
 
 // CreateTable create table if not exists
 func CreateTable(name string) error {
-	return db.CreateTable(name)
+	return rethink.CreateTable(name)
 }
 
 // Find base find query
 func Find(q r.Term, result interface{}) error {
-	return db.Find(q, result)
+	return rethink.Find(q, result)
 }
 
 // FindOne base findone query
 func FindOne(table string, result interface{}) error {
-	return db.FindOne(r.Table(table), result)
+	return rethink.FindOne(r.Table(table), result)
 }
 
 // Get base get query
 func Get(table, id string, result interface{}) error {
-	return db.FindOne(r.Table(table).Get(id), result)
+	return rethink.FindOne(r.Table(table).Get(id), result)
 }
 
 // Create base create query
 func Create(table string, input interface{}) (string, error) {
-	return db.Create(r.Table(table).Insert(input))
+	return rethink.Create(r.Table(table).Insert(input))
 }
 
 // Update base create query
 func Update(table, id string, input interface{}) error {
 	// modifying without checking if the record exists is fine with rethinkDB
 	// update mechanism it will skip the non existent record
-	return db.Update(r.Table(table).Get(id).Update(input))
+	return rethink.Update(r.Table(table).Get(id).Update(input))
 }
 
 // Remove base create query
 func Remove(table, id string) error {
-	return db.Remove(r.Table(table).Get(id).Delete())
+	return rethink.Remove(r.Table(table).Get(id).Delete())
 }
